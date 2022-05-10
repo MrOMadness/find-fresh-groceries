eae6c9ed6ed605901b79ad0cfaa197c2db44d7a3
MrOMadness <nicholas.stanciosaka@gmail.com>
2022-05-10 14:36:22 +0700

    add style page

---
+++ lib/styles.dart
@@ -0,0 +1,14 @@
+// Style page
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const String robotoFont = 'roboto';
+  static const int greenMain = 0xff5BB98B;
+  static const int inactiveGrey = 0xff5BB98B;
+
+  static const TextStyle roboto10Bold = TextStyle(
+    fontFamily: robotoFont,
+    fontWeight: FontWeight.w700,
+    fontSize: 10,
+  );
+}
