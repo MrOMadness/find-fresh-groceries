6f2e80961ec52c063a8e93e70fa5b18fbef9d2d7
MrOMadness <nicholas.stanciosaka@gmail.com>
2022-05-10 15:56:55 +0700

    get api

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:find_fresh_groceries/nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
+  // Get API data from https://randomuser.me/api/
+  var url = Uri.parse('https://randomuser.me/api/');
+  var response = await http.get(url);
+
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
+
   WidgetsFlutterBinding.ensureInitialized();
 
   // Remove android status bar
@@ -20,6 +28,7 @@ class MyApp extends StatelessWidget {
       title: 'Find Fresh Groceries', // Title of the app
       theme: ThemeData(
         primarySwatch: Colors.lightBlue, // Primary theme color
+        scaffoldBackgroundColor: Colors.white,
       ),
       home: const NavBar(),
     );
