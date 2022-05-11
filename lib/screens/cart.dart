2ac638b3f025385e231d5154734d56efe5a573f7
MrOMadness <nicholas.stanciosaka@gmail.com>
2022-05-12 03:04:15 +0700

    clean code and add comments

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -6,6 +6,7 @@ import 'package:find_fresh_groceries/public/functions.dart';
 import 'package:find_fresh_groceries/templates/signature_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -23,20 +24,24 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        // app bar
         appBar: const SignatureAppBar(
           title: 'Cart Product',
         ),
+        // body
         body: Consumer<CartModel>(
           builder: (context, cart, child) {
+            // detail list view of cart
             return ListView(
                 padding: const EdgeInsets.fromLTRB(25, 25, 25, 95),
                 children: getProductList(cart));
           },
         ),
+        // floacting action button
         floatingActionButton: Consumer<CartModel>(
           builder: (context, cart, child) {
             return SizedBox(
-              width: MediaQuery.of(context).size.width - 32,
+              width: MediaQuery.of(context).size.width - 32, // full width
               height: 65,
               child: FloatingActionButton(
                 elevation: 0,
@@ -45,17 +50,21 @@ class _CartScreenState extends State<CartScreen> {
                     borderRadius: BorderRadius.all(Radius.circular(10))),
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 22.0),
+                  // row. consists of total detail and checkout text
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
+                      // row 1: cart total detail
                       Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          // column 1: total {x} item
                           Text(
                             'Total ' + cart.countNoMax() + ' Item',
                             style: Styles.roboto16BoldWhite,
                           ),
+                          // line
                           Container(
                             margin: const EdgeInsets.all(2),
                             padding: const EdgeInsets.all(0),
@@ -65,12 +74,14 @@ class _CartScreenState extends State<CartScreen> {
                               color: Colors.white,
                             )),
                           ),
+                          // total in rupiah
                           Text(
                             'Rp ' + convertDoubleToCurrency(cart.totalPrice()),
                             style: Styles.roboto16BoldWhite,
                           ),
                         ],
                       ),
+                      // row 2: checkout text
                       const Text(
                         'Checkout',
                         style: Styles.roboto16BoldWhite,
@@ -91,9 +102,8 @@ class _CartScreenState extends State<CartScreen> {
                   List<dynamic> storedArray = [];
                   SharedPreferences pref =
                       await SharedPreferences.getInstance();
-
+                  // Get from shared if exist
                   if (pref.getString('transactions') != null) {
-                    // Get from shared
                     var listString = pref.getString('transactions');
                     List<dynamic> list = jsonDecode(listString!);
 
@@ -108,6 +118,19 @@ class _CartScreenState extends State<CartScreen> {
 
                   await pref.setString('transactions',
                       storedArray.toString()); // change to string
+
+                  // success toast
+                  Fluttertoast.showToast(
+                      msg: "Transaction saved successfully",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.grey.shade600,
+                      textColor: Colors.white,
+                      fontSize: 16.0);
+
+                  // clean cart
+                  cart.deleteAll();
                 },
               ),
             );
@@ -115,11 +138,15 @@ class _CartScreenState extends State<CartScreen> {
         ));
   }
 
+  // widget list of product
   List<Widget> getProductList(cart) {
     List<Widget> productList = [];
 
+    // loop on every categories
     cart.countPerElement().forEach((key, value) {
+      // add product on the category
       productList.add(
+        // border
         Container(
             decoration: BoxDecoration(
               color: Colors.white,
@@ -129,12 +156,15 @@ class _CartScreenState extends State<CartScreen> {
             ),
             margin: const EdgeInsets.only(top: 10),
             height: 130,
+            // column consists of all data
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
+                // column 1: heading row (title and delete)
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
+                    // row 1: title
                     Padding(
                       padding: const EdgeInsets.only(left: 15.0),
                       child: Text(
@@ -142,6 +172,7 @@ class _CartScreenState extends State<CartScreen> {
                         style: Styles.roboto16BoldLowBlack,
                       ),
                     ),
+                    // row 2: delete
                     Padding(
                       padding: const EdgeInsets.only(right: 5),
                       child: IconButton(
@@ -156,12 +187,14 @@ class _CartScreenState extends State<CartScreen> {
                     ),
                   ],
                 ),
+                // column 2: datas
                 Padding(
                   padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
+                      // row 1: image
                       Container(
                         decoration: BoxDecoration(
                           color: const Color(Styles.imgGrey),
@@ -177,10 +210,12 @@ class _CartScreenState extends State<CartScreen> {
                         child: Image.network(key.picture,
                             height: 60.0, width: 60.0),
                       ),
+                      // row 2: price and rating
                       Column(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          // column 1: price
                           Padding(
                             padding: const EdgeInsets.fromLTRB(4, 6, 4, 6),
                             child: Text(
@@ -191,6 +226,7 @@ class _CartScreenState extends State<CartScreen> {
                               style: Styles.roboto16MediumLowBlack,
                             ),
                           ),
+                          // column 2: rating
                           Padding(
                             padding: const EdgeInsets.only(bottom: 8.0),
                             child: RatingBarIndicator(
@@ -206,9 +242,12 @@ class _CartScreenState extends State<CartScreen> {
                           ),
                         ],
                       ),
+                      // column 3: expended make next column right aligned
                       Expanded(child: Container()),
+                      // column 4: data manipulation (add and substract)
                       Row(
                         children: [
+                          // row 1: substract
                           ElevatedButton(
                               style: ButtonStyle(
                                   padding: MaterialStateProperty.all(
@@ -225,6 +264,7 @@ class _CartScreenState extends State<CartScreen> {
                                     borderRadius: BorderRadius.circular(10.0),
                                   ))),
                               onPressed: () {
+                                // function to substract selected element by 1
                                 cart.reduceElement(key);
                               },
                               child: Container(
@@ -243,7 +283,9 @@ class _CartScreenState extends State<CartScreen> {
                                   ),
                                 ),
                               )),
+                          // row 2: qty of product
                           Text(value.toString()),
+                          // row 3: add
                           ElevatedButton(
                               style: ButtonStyle(
                                   padding: MaterialStateProperty.all(
@@ -260,6 +302,7 @@ class _CartScreenState extends State<CartScreen> {
                                     borderRadius: BorderRadius.circular(10.0),
                                   ))),
                               onPressed: () {
+                                // function to add
                                 cart.add(key);
                               },
                               child: Container(
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -5,24 +5,30 @@ import 'package:find_fresh_groceries/templates/signature_appbar.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
+  // parameter to get user
   final User user;
   const ProfileScreen({Key? key, required this.user}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // signature app bar with title 'profile'
       appBar: const SignatureAppBar(
         title: 'Profile',
       ),
+      // creates a stack
       body: Stack(
         children: [
+          // stack 1: upper background
           Container(
             height: 163,
             color: const Color(Styles.backgroundGrey),
           ),
+          // stack 2: main content
           Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              // column 1: picture
               Container(
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
@@ -35,12 +41,15 @@ class ProfileScreen extends StatelessWidget {
                       height: 150.0, width: 150.0, fit: BoxFit.fill),
                 ),
               ),
+              // column 2: name
               Center(
                   child: Text(
                 user.fullName,
                 style: Styles.roboto16BoldLowBlack,
               )),
+              // column 3: user data
               Expanded(
+                // background
                 child: Container(
                   margin: const EdgeInsets.only(top: 55),
                   decoration: const BoxDecoration(
@@ -52,12 +61,16 @@ class ProfileScreen extends StatelessWidget {
                   child: ListView(
                     padding: const EdgeInsets.only(top: 55),
                     children: [
+                      // username
                       ProfileBoxTemplate(
                           user.userName, 'assets/images/id_icon.png'),
+                      // email
                       ProfileBoxTemplate(
                           user.email, 'assets/images/email_icon.png'),
+                      // phone
                       ProfileBoxTemplate(
                           user.phone, 'assets/images/phone_icon.png'),
+                      // location
                       Container(
                         decoration: BoxDecoration(
                           color: Colors.white,
