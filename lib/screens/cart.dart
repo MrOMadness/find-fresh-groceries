76d999f31766b1cce2dfddd3ea31d4ed96c2dc80
MrOMadness <nicholas.stanciosaka@gmail.com>
2022-05-11 16:44:54 +0700

    add save transaction

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:find_fresh_groceries/models/cart.dart';
+import 'package:find_fresh_groceries/models/transaction.dart';
+import 'package:find_fresh_groceries/public/functions.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -70,10 +75,37 @@ class _CartScreenState extends State<CartScreen> {
         floatingActionButton: Consumer<CartModel>(
           builder: (context, cart, child) {
             return FloatingActionButton(
-              onPressed: () {
-                //TODO: save
+              onPressed: () async {
+                // Penyimpanan transaksi produk yang terdapat pada page “cart” dibuat dinamis  dalam local storage.
+                Transaction trx = Transaction(
+                    totalPrice: convertDoubleToCurrency(cart.totalPrice()),
+                    time: DateTime.now().toString(),
+                    trxId: 'trx' +
+                        DateTime.now().millisecondsSinceEpoch.toString() +
+                        cart.totalPrice().toStringAsFixed(0));
+
+                // Save to shared
+                List<dynamic> storedArray = [];
+                SharedPreferences pref = await SharedPreferences.getInstance();
+
+                if (pref.getString('transactions') != null) {
+                  // Get from shared
+                  var listString = pref.getString('transactions');
+                  List<dynamic> list = jsonDecode(listString!);
+
+                  for (var json in list) {
+                    storedArray.add(
+                        jsonEncode(json)); // Add transactions to stored array
+                  }
+                }
+
+                storedArray.add(
+                    jsonEncode(trx.toJson())); // add most recent transaction
+
+                await pref.setString(
+                    'transactions', storedArray.toString()); // change to string
               },
-              child: Text(cart.totalPrice().toString()),
+              child: Text(convertDoubleToCurrency(cart.totalPrice())),
             );
           },
         ));
